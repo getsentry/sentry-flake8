9f9a6820fb5c2059c939f81745ae49aa4a8cec33
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-08-10 11:48:52 -0700

    feat: Add linting for sentry.utils.json (#15)

---
+++ tests/cases/b317.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+import json
+import simplejson
+from json import loads, load
+from simplejson import JSONDecoder, JSONDecodeError, _default_encoder
+import sentry.utils.json as good_json
+from sentry.utils.json import JSONDecoder, JSONDecodeError
+
+
+def bad_code():
+    a = json.loads("''")
+    b = simplejson.loads("''")
+    c = loads("''")
+    d = load()
